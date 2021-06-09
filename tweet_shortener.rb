ed1aa4c2e5cac126612553370899fbc4b51ff620
“Tony <adrprogramming@gmail.com>
2021-06-09 12:05:52 -0600

    commit tree 46a0c753b5f92bf731c62332acaebbbbe19c757b

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+
+dictionary = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+}
+
+def dictionary(dictionary)
+    dictionary
+end
+
+def word_substituter(string)
+    new_string = string.split()
+end
